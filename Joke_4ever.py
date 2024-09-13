ea3f8fe7953e27dc97d321a0cf7b1c9b9469338d
b.smirnov <besm1@yandex.ru>
2024-09-13 18:49:41 +0300

    Вторая версия

--- Joke_4ever.py
+++ Joke_4ever.py
@@ -1,5 +1,3 @@
-from idlelib.colorizer import color_config
-from os import cpu_count
 from tkinter import *
 from random import randint
 
@@ -8,8 +6,8 @@ class Joke:
 
     def __init__(self):
         self.root = Tk()
-        self.root.title("It's NO joke!")
-        self.root.geometry("300x250+800+400")
+        self.root.title("Опрос отдела кадров")
+        self.root.geometry("300x250+800+600")
 
         self.btn_yes = Button(text = 'Да', command=self.button_click, height=1, width=6, padx=5)
         self.btn_yes.place(x=75, y=112)
@@ -18,14 +16,16 @@ class Joke:
         self.btn_no.place(x=165, y=112)
         self.btn_no.bind('<Enter>', self.entered)
 
-        # self.root.bind('<Motion>', self.get_mouse_coord)
+        self.question = Label(text='Вы довольны\nуровнем своей зарплаты?', font=("Times New Roman", 14),
+                               foreground='blue')
+        self.question.pack(side=TOP)
 
-        self.label = Label(text='Ваш честный ответ будет\nпередан в отдел кадров.', font=("Times New Roman", 16))
-        self.question = Label(text='Вы довольны\nуровнем своей зарплаты?', font=("Times New Roman", 14), anchor='s')
-        self.question.pack()
+        self.label = Label(text='Спасибо за участие в опросе!\nВаш честный ответ будет\nпередан в отдел кадров.'
+                           , font=("Times New Roman", 16, 'bold'), foreground='red')
 
         self.btn_no_home = None
         self.in_place = True
+
 
         mainloop()
 
@@ -52,12 +52,11 @@ class Joke:
         btn_new = self.get_new_coords(btn)
         btn_new_coords = (btn_new[0],btn_new[1], btn_new[0] + self.btn_no.winfo_width()
                                 , btn_new[1] +self.btn_no.winfo_height())
-        # while (btn_new[0] <= btn1.winfo_x() + btn1.winfo_width() and  btn_new[0] + btn.winfo_width() >= btn1.winfo_x()
-        #         and btn_new[1] <= btn1.winfo_y() + btn1.winfo_height() and  btn_new[1] + btn.winfo_height() >= btn1.winfo_y()):
-        while any(intersection(btn_new_coords, w_coords(w_)) for w_ in (self.btn_yes, self.question))  :
+        while any(intersection(btn_new_coords, w_) for w_ in (self.btn_yes, self.question, self.btn_no_home))  :
             btn_new = self.get_new_coords(btn)
             btn_new_coords = (btn_new[0],btn_new[1], btn_new[0] + self.btn_no.winfo_width()
                                 , btn_new[1] +self.btn_no.winfo_height())
+        print(btn_new)
         btn.place(x=btn_new[0], y=btn_new[1])
 
     def button_click(self):
@@ -68,12 +67,20 @@ def w_coords(w:Widget):
     return w.winfo_x(), w.winfo_y(), w.winfo_x() + w.winfo_width(), w.winfo_y() + w.winfo_height()
 
 def intersection(coords1, coords2) -> bool:
+    print(f'Intersection: coords1 = {coords1}, coords2 = {coords2}')
+    if coords1.__class__.__bases__[0].__name__ == 'Widget':
+        coords1 = w_coords(coords1)
+    if coords2.__class__.__bases__[0].__name__ == 'Widget':
+        coords2 = w_coords(coords2)
+    print(f'Intersection: coords1 = {coords1}, coords2 = {coords2}')
     if (isinstance(coords1, (tuple, list, set)) and len(coords1) == 4
             and isinstance(coords2, (tuple, list, set)) and len(coords2) == 4):
-        return (coords1[0] <= coords2[2] and coords1[2] >= coords2[0]
+        ret = (coords1[0] <= coords2[2] and coords1[2] >= coords2[0]
                 and coords1[1] <= coords2[3] and coords1[3] >= coords2[1])
+        print(f'ret: {ret}')
+        return ret
     else:
-        return False
+        return True
 
 if __name__ == '__main__':
     joke = Joke()
