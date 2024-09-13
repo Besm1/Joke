f21ae025d98de47c97853145a0ec3df28e0bd943
b.smirnov <besm1@yandex.ru>
2024-09-13 14:20:32 +0300

    Вторая версия

--- Joke_4ever.py
+++ Joke_4ever.py
@@ -1,13 +1,8 @@
 from tkinter import *
 from random import randint
-from datetime import datetime as dt
+from copy import deepcopy
 
 class Joke:
-
-    # TIME_TO_RUN = 15    # время (сек), в течение которого кнопка убегает от мыши
-    #
-    # start_time = 0  # Объявление атрибута класса, обозначающего время начала "ловли" кнопки юзером
-    # btn_state = 0     # предполагаем три состояния кнопки: 0 - не начала убегать, 1 - убегает и 2 - остановилась
 
     root = None
 
@@ -23,19 +18,15 @@ class Joke:
         self.btn_no.place(x=165, y=112)
         self.btn_no.bind('<Enter>', self.entered)
 
-        self.root.bind('<Motion>', self.get_mouse_coord)
+        # self.root.bind('<Motion>', self.get_mouse_coord)
 
         self.img = PhotoImage(file='smile.png')
         self.label = Label(text='С первым апреля!', font=("Times New Roman", 14), image=self.img, compound="bottom")
 
         self.btn_no_home = None
+        self.in_place = True
 
         mainloop()
-        print('start')
-
-    def get_mouse_coord(self, event):
-        self.mouse_at = (event.x, event.y)
-
 
 
     def entered(self, event):
@@ -43,8 +34,12 @@ class Joke:
             self.btn_no_home = (self.btn_no.winfo_x(),
                                 self.btn_no.winfo_y(),
                                 self.btn_no.winfo_x() + self.btn_no.winfo_width(),
-                                self.btn_no.winfo_y()  + self.btn_no.winfo_width())
-        self.button_jump(event, self.btn_no, self.btn_yes)
+                                self.btn_no.winfo_y()  + self.btn_no.winfo_height())
+        if self.in_place:
+            self.button_jump(event, self.btn_no, self.btn_yes)
+        else:
+            self.btn_no.place(x=self.btn_no_home[0], y=self.btn_no_home[1])
+        self.in_place = not self.in_place
 
 
     def get_new_coords(self, btn):
@@ -52,18 +47,16 @@ class Joke:
                 randint(0, self.root.winfo_height() - btn.winfo_height() - 1))
 
     def button_jump(self, event, btn:Button, btn1:Button):
-        mouse_x = btn.winfo_x() + event.x
-        mouse_y = btn.winfo_y() + event.y
-        x_new , y_new = self.get_new_coords(btn)
-        while ((x_new <= mouse_x <= x_new + btn.winfo_width() and y_new <= mouse_y <= y_new + btn.winfo_height()) or
-                (x_new <= btn1.winfo_x() + btn1.winfo_width() and  x_new + btn.winfo_width() >= btn1.winfo_x()
-                and y_new <= btn1.winfo_y() + btn1.winfo_height() and  y_new + btn.winfo_height() >= btn1.winfo_y())):
-            x_new, y_new = self.get_new_coords(btn)
-        btn.place(x=x_new, y=y_new)
+        btn_new = self.get_new_coords(btn)
+        while (btn_new[0] <= btn1.winfo_x() + btn1.winfo_width() and  btn_new[0] + btn.winfo_width() >= btn1.winfo_x()
+                and btn_new[1] <= btn1.winfo_y() + btn1.winfo_height() and  btn_new[1] + btn.winfo_height() >= btn1.winfo_y()):
+            btn_new = self.get_new_coords(btn)
+        btn.place(x=btn_new[0], y=btn_new[1])
 
     def button_click(self):
 
         self.label.pack(expand=1)
+
 
 if __name__ == '__main__':
     joke = Joke()
